367071678dea0178da4125f509bdeee3402e3436
ashwini121 <satheashwini963@gmail.com>
2024-04-16 16:03:14 +0000

    initial commit

--- main.py
+++ main.py
@@ -170,4 +170,7 @@ def ask_gemini(question, data):
   )
   return response.text
 
-if __name__ == '__main__':
+  if __name__ == '__main__':
+      app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+
